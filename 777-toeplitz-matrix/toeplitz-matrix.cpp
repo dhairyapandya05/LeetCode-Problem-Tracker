7d6b54fd1627e1c5b40c0c180bd9a6f9f4cc9108
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-12 02:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (5.02%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        unordered_map<int,int>mp;
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mp.find(i-j)==mp.end()){
+                    mp[i-j]=matrix[i][j];
+                }
+                else{
+                    if(mp[i-j]!=matrix[i][j]) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
