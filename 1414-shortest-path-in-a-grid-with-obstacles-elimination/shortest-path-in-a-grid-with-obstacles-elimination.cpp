cefb76258ac274959bc50a8b329c8487300bbf3f
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-12 02:14:27 +0530

    Time: 595 ms (10.52%) | Memory: 161.2 MB (7.86%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,55 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    vector<vector<int>> dir{{-1,0},{1,0},{0,1},{0,-1}};
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        queue<vector<int>> que;
-
-        int i = 0, j = 0;
-        que.push({i, j, k});
-
-        bool visited[41][41][1601];
-        // vector<vector<vector<bool>>> visited(m, vector<vector<bool>>(n,
-        // vector<bool>(k)));
-        memset(visited, false, sizeof(visited));
-
-        int steps = 0;
-        while (!que.empty()) {
-            int size = que.size();
-            while (size--) {
-                vector<int> temp = que.front();
-                que.pop();
-                int curr_i = temp[0];
-                int curr_j = temp[1];
-                int obs = temp[2];
-
-                if (curr_i == m - 1 && curr_j == n - 1)
-                    return steps;
-
-                for (vector<int>& dir : directions) {
-
-                    int new_i = curr_i + dir[0];
-                    int new_j = curr_j + dir[1];
-
-                    if (new_i < 0 || new_i >= m || new_j < 0 || new_j >= n)
-                        continue;
-
-                    if (grid[new_i][new_j] == 0 &&
-                        !visited[new_i][new_j][obs]) {
-                        que.push({new_i, new_j, obs});
-                        visited[new_i][new_j][obs] = true;
-                    } else if (grid[new_i][new_j] == 1 && obs > 0 &&
-                               !visited[new_i][new_j][obs - 1]) {
-                        que.push({new_i, new_j, obs - 1});
-                        visited[new_i][new_j][obs - 1] = true;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<vector<int>> qe;
+        int steps=0;
+        bool visited[41][41][16001];
+        memset(visited,false,sizeof(visited));
+        
+        qe.push({0,0,k});
+        visited[0][0][k]=true;
+        while(!qe.empty()){
+            int sz=qe.size();
+            while(sz--){
+                auto tp=qe.front();
+                qe.pop();
+                int curr_i=tp[0];
+                int curr_j=tp[1];
+                int obst=tp[2];
+                if(curr_i==n-1 and curr_j==m-1) return steps;
+                for(auto d:dir){
+                    int newx=curr_i+d[0];
+                    int newy=curr_j+d[1];
+                    if(newx<0 or newy<0 or newx>=n or newy>=m) continue;
+                    if(grid[newx][newy]==0 and visited[newx][newy][obst]==false ){
+                        visited[newx][newy][obst]=true;
+                        qe.push({newx,newy,obst});
+                    }
+                    else if(grid[newx][newy]==1 and obst>0 and visited[newx][newy][obst-1]==false){
+                        visited[newx][newy][obst-1]=true;
+                        qe.push({newx,newy,obst-1});
                     }
                 }
             }
             steps++;
         }
-
         return -1;
     }
 };
