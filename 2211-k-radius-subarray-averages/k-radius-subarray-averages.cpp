a39e84a45dbf46943b7a6d0c4be64712efae713a
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-19 17:51:04 +0530

    Time: 14 ms (19.90%) | Memory: 142.1 MB (28.65%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> ans(n, -1);
-        if (((2 * k) + 1) > n)
-            return ans;
-        int i = k;
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        if(((2*k)+1)>n) return ans;
+        int i=k;
         vector<long long> prefix(n, 0);
         prefix[0] = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
         }
-        for (int i = k; i <= n - k - 1; i++) {
-            long long sum = prefix[i + k];
-            if (i - k > 0) {
-                sum -= prefix[i - k - 1];
-            }
-            ans[i] = sum / ((2 * k) + 1);
+        for(int i=k;i<=n-k-1;i++){
+            long long int leftlimit=(i-k-1>=0)?prefix[i-k-1]:0;
+            long long int sum=prefix[i+k]-leftlimit;
+            ans[i]=sum/((2*k)+1);
         }
         return ans;
     }
