1d8e5fdf93446512d54e5d599e824515d9070071
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-22 06:05:03 +0530

    Time: 10 ms (48.49%) | Memory: 13.3 MB (82.31%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n=customers.length();
-        vector<int>prefixN(n,0);
-        vector<int>suffixY(n,0);
+        int n = customers.length();
+        int rightPanelty = 0;
+        int leftPanelty = 0;
 
-        if(customers[0]=='N')
-        prefixN[0]=1;
-
-        if(customers[n-1]=='Y')
-        suffixY[n-1]=1;
-
-        for(int i=1;i<n;i++){
-            prefixN[i]=prefixN[i-1]+((customers[i]=='N')?1:0);
-        }
-        for(int i=n-2;i>=0;i--){
-            suffixY[i]=suffixY[i+1]+((customers[i]=='Y')?1:0);
-        }
-        int miniHr=0;
-        int minipanalty=INT_MAX;
-        for(int i=0;i<=n;i++){
-            int prefixcollisions=0;
-            int suffixcollisions=0;
-
-            if(i-1>=0)
-            prefixcollisions=prefixN[i-1];
-
-            if(i<n)
-            suffixcollisions=suffixY[i];
-
-            int totalpanalty=prefixcollisions+suffixcollisions;
-
-            if(totalpanalty<minipanalty){
-                minipanalty=totalpanalty;
-                miniHr=i;
+        for (int i = 0; i < n; i++) {
+            if (customers[i] == 'Y') {
+                rightPanelty++;
             }
-
-
+        }
+        int miniHr = 0;
+        int totalPanelty;
+        int minipanelty = rightPanelty;
+        for (int i = 0; i < n; i++) {
+            totalPanelty = rightPanelty + leftPanelty;
+            if (totalPanelty < minipanelty) {
+                minipanelty = totalPanelty;
+                miniHr = i;
+            }
+            if (customers[i] == 'N') {
+                leftPanelty++;
+            } else if (customers[i] == 'Y') {
+                rightPanelty--;
+            }
+        }
+        if (leftPanelty < minipanelty) {
+            miniHr = n;
         }
         return miniHr;
     }
