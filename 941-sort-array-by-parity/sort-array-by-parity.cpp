e912c13dee4476a41be14e6ca25176db31991d39
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-25 08:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (97.06%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
+    static bool comparator(int a, int b) {
+        return a%2 < b%2; //a%2 will be 0 if it's even and since we do a%2 < b%2, hence a will come first if it's even
+    }
+    
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i = 0, n = nums.size(), j = 0;
-        // vector<int>temp (n);
-
-        while (i < n) {
-            if (nums[i] % 2 == 0) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
-            i++;
-        }
+        sort(begin(nums), end(nums), comparator);
+        
         return nums;
     }
 };
