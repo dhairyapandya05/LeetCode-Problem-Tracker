77ba6ad15f75e097665b528fb7638c44b052e481
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-01-27 19:20:19 +0530

    Time: 1210 ms (5.30%) | Memory: 128.1 MB (10.61%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int minele=nums[0];
-        int operations=0;
-        for(int i=n-1;i>0;i--){
-            if(nums[i]==nums[i-1]) continue;
-            operations+=(n-i);
+        map<int, int> mp;
+        for (auto it : nums) {
+            mp[it]++;
         }
-        return operations;
+        int totaloperations = 0;
+        int operations=0;
+
+        int n = mp.size();
+        for (auto it = prev(mp.end()); it != mp.begin(); it--) {
+            cout << (it->first) << "d ";
+            operations+=it->second;
+            totaloperations += operations;
+        }
+        return totaloperations;
     }
 };
