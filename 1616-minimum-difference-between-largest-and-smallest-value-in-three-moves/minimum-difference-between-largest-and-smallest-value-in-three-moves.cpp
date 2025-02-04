e78a4c58ad952896d5b88db91aa28d4eb68c6ed7
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-04 08:41:37 +0530

    Time: 3 ms (94.56%) | Memory: 42.2 MB (96.95%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -3,7 +3,9 @@ public:
     int minDifference(vector<int>& nums) {
         int n=nums.size();
         if(n<=4) return 0;
-        sort(nums.begin(),nums.end());
+        partial_sort(nums.begin(),nums.begin()+4,nums.end()); //n*log(4)
+        nth_element(nums.begin()+4,nums.begin()+(n-4),nums.end()); //n*log(4) it implements partisiioning technique so tbe time compleity is O(n)
+        sort((nums.begin()+n-4),nums.end());
         int diff=INT_MAX;
         diff=min(diff,nums[n-4]-nums[0]);
         diff=min(diff,nums[n-3]-nums[1]);
