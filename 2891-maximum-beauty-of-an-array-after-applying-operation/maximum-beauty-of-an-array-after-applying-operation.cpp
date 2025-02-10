0c9d6602e87f37eb959454fe6c22e0efa0b372b9
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-11 02:24:17 +0530

    Time: 90 ms (22.53%) | Memory: 105 MB (83.80%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int maximumBeauty(vector<int>& nums, int k) {
-        vector<pair<int,int>>intervals;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            intervals.push_back({nums[i]-k,nums[i]+k});
-        }
-
-        sort(intervals.begin(),intervals.end());
-        deque<int>de;
-        int maxBeauty=0;
-        for(int i=0;i<n;i++){
-            while(!de.empty() and de.front()<intervals[i].first){
-                de.pop_front();
+    int binarysearch(vector<int>& nums,int target){
+        int s=0;
+        int ans=0;
+        int e=nums.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]>target){
+                e=mid-1;
             }
-            de.push_back(intervals[i].second);
-            maxBeauty=max(maxBeauty,(int)de.size());
+            else{
+                ans=mid;
+                s=mid+1;
+            }
+        }
+        return ans;
+
+    }
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int right = 0;
+        int maxBeauty = 0;
+
+        for(int left = 0; left<nums.size(); left++){
+            int x=nums[left];
+            int y=x+(2*k);
+           int right=binarysearch(nums,y);
+  
+            maxBeauty = max(maxBeauty,right-left+1);
         }
         return maxBeauty;
     }
