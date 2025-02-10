92f5bba336680ae15b86d419e3d6c43938413c0c
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-11 00:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 126 MB (97.85%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
+    bool binarySearch(vector<int>& invalidIdx,vector<int>& query){
+        int low=query[0]+1;
+        int high=query[1];
+        int s=0,e=invalidIdx.size()-1;
+        while(s<=e){
+            int mid=(s+(e-s)/2);
+            if(invalidIdx[mid]<low) s=mid+1;
+            else if(invalidIdx[mid]>high) e=mid-1;
+            else{
+                return true;
+            }
+        }
+        return false;
+
+    }
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> prefix(n + 1, 0);
-
-        // Build prefix sum array to mark invalid indices
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + (nums[i] % 2 == nums[i - 1] % 2);
+        vector<int> invalidIdx;
+        int m=queries.size();
+        int j=1;
+        while(j<n){ 
+            if(nums[j]%2==nums[j-1]%2){
+                invalidIdx.push_back(j);
+            }
+            j++;
         }
+        vector<bool> result(m,false);
 
-        vector<bool> result;
-        for (const auto& q : queries) {
-            int l = q[0], r = q[1];
-            result.push_back(prefix[r] == prefix[l]);
+        for(int i=0;i<m;i++){
+            bool r1=binarySearch(invalidIdx,queries[i]);
+             result[i]=!r1;
         }
         return result;
     }
