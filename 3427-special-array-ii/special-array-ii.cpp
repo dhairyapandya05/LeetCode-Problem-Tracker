bd5ff4a4246638cc04266785e0d0e4e750910958
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-10 23:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (65.16%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,26 +2,17 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> validRightMostIdx(n,0);
-        int m=queries.size();
-        int i=0,j=0;
-        while(i<n){
-            if(j<i){    // safety cheque
-                j=i;
-            }
-            while(j+1<n and nums[j]%2!=nums[j+1]%2){
-                j++;
-            }
-            validRightMostIdx[i]=j; 
-            i++;
+        vector<int> prefix(n + 1, 0);
+
+        // Build prefix sum array to mark invalid indices
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + (nums[i] % 2 == nums[i - 1] % 2);
         }
-        vector<bool>result(m,false);
-        for(int i=0;i<m;i++){
-            int start=queries[i][0];
-            int end=queries[i][1];
-            if(end<=validRightMostIdx[start]){
-                result[i]=true;
-            }
+
+        vector<bool> result;
+        for (const auto& q : queries) {
+            int l = q[0], r = q[1];
+            result.push_back(prefix[r] == prefix[l]);
         }
         return result;
     }
