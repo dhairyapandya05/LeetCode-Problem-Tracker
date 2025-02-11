8883679a7b29afc7bf4f559ddce5fb10037e1a15
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-11 10:28:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (15.22%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& nums) {
         int n=nums.size();
-        vector<int>prefix=nums;
-        vector<int>suffix=nums;
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],prefix[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            
-            suffix[i]=min(suffix[i],suffix[i+1]);
-        }
-        int chunks=0;
+        int cumSum=0,origSum=0,chunks=0;
         for(int i=0;i<n;i++){
-            int pahaleaeKaMax=(i-1>=0)?prefix[i-1]:-1;
-            int baadKaMin=suffix[i];
-            if(pahaleaeKaMax<baadKaMin){
+            cumSum+=nums[i];
+            origSum+=i;
+            if(origSum==cumSum){
                 chunks++;
             }
         }
