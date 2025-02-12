02840c001e8d37222fd9a56ae453b7cf83c42797
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-12 10:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (55.12%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -4,16 +4,19 @@ public:
         int n = values.size();
         if (n < 2)
             return values[0] + values[1] + 0 - 1;
-        vector<int> prefixMax(n);
-        prefixMax[0] = values[0];
+        int maxValueIplusI=values[0]+0;
+        int result=0;
         for (int i =1; i <n; i++) {
-            prefixMax[i] = max(prefixMax[i - 1], values[i] + i);
+            int x=values[i]-i;
+            int y=maxValueIplusI;
+            result = max(result,x+y);
+            maxValueIplusI=max(maxValueIplusI,values[i]+i);
         }
-        int maxScore = INT_MIN;
-        for (int i = 1; i < n; i++) {
-            maxScore = max(maxScore, prefixMax[i - 1] + values[i] - i);
-        }
+        // int maxScore = INT_MIN;
+        // for (int i = 1; i < n; i++) {
+        //     maxScore = max(maxScore, prefixMax[i - 1] + values[i] - i);
+        // }
 
-        return maxScore;
+        return result;
     }
 };
