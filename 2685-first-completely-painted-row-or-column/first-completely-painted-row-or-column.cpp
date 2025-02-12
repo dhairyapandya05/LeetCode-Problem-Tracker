eca9aff68f3acf7b6cfe598ea85339852b0bd6b5
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-02-13 01:10:22 +0530

    Time: 95 ms (62.68%) | Memory: 163 MB (65.81%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int nr = mat.size();
-        int nc = mat[0].size();
-        std::unordered_map<int, pair<int,int>> coords;
-        std::vector<int> visitedr(nr,0),visitedc(nc,0);
-        for(int r=0; r<nr; r++)
+        int n = mat.size();
+        int m = mat[0].size();
+        unordered_map<int,int> mp;
+        vector<int> visitedr(n,0),visitedc(m,0);
+        int t=arr.size();
+        for(int i=0;i<t;i++){
+            int val=arr[i];
+            mp[val]=i;
+        }
+        int minIdx=t-1;
+        for(int r=0; r<n; r++)
         {
-            for(int c=0; c<nc; c++)
+            int lastIdx=INT_MIN;
+            for(int c=0; c<m; c++)
             {
-                coords[mat[r][c]] = {r,c};
+                int val=mat[r][c];
+                int idx=mp[val];
+                lastIdx=max(lastIdx,idx);
             }
+            minIdx=min(minIdx,lastIdx);
         }
-        int e=0;
-        for(e=0; e<arr.size(); e++)
+
+
+        for(int c=0; c<m; c++)
         {
-            auto [r,c] = coords[arr[e]];
-            if(++visitedr[r]==nc || ++visitedc[c]==nr) return e;
+            int lastIdx=INT_MIN;
+            for(int r=0; r<n; r++)
+            {
+                int val=mat[r][c];
+                int idx=mp[val];
+                lastIdx=max(lastIdx,idx);
+            }
+            minIdx=min(minIdx,lastIdx);
         }
-        return arr.size();
+        return minIdx;
     }
 };
