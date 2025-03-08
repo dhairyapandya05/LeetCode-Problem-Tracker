03af4188a8e787d33a044afbd28206ae497bd1f3
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-03-08 20:15:14 +0530

    Time: 531 ms (5.07%) | Memory: 145.5 MB (74.83%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        int n = changed.size();
-        vector<int> result;
-        if (n % 2 == 1)
-            return {};
-        unordered_map<int, int> mp;
-        for (auto& it : changed) {
+        unordered_map<int,int>mp;
+        for(auto it:changed){
             mp[it]++;
         }
-        sort(changed.begin(), changed.end());
-        for (int i = 0; i < n; i++) {
-            int ele = changed[i];
-            int target = 2 * ele;
-            if (mp[ele] == 0)
-                continue;
-            if (mp.find(target) == mp.end() or mp[target] == 0)
-                return {};
-            result.push_back(ele);
-            mp[ele]--;
-            mp[target]--;
+        sort(changed.begin(),changed.end());
+        int n=changed.size();
+        if(n%2!=0) return {};
+        vector<int>original;
+        for(int i=0;i<n;i++){
+            if(mp[changed[i]]==0) continue;
+            if(mp[(2*changed[i])]==0) return {};
+            cout<<changed[i]<<" '";
+            original.push_back(changed[i]);
+            mp[changed[i]]--;
+            mp[(2*changed[i])]--;
         }
-        return result;
+        return original;
     }
 };
