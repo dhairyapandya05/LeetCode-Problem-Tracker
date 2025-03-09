0e9d4da9db110fff1e232c03c5737d6e919a94f9
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-03-10 03:35:03 +0530

    Time: 247 ms (28.79%) | Memory: 110.8 MB (38.02%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -4,15 +4,15 @@ public:
         set<int> st(nums.begin(), nums.end());
         vector<int> temp(st.begin(), st.end());
 
-        int n = temp.size();
         int m = nums.size();
-
+        int n = temp.size();
         int result = INT_MAX;
-
+        
         for (int i = 0, j = 0; i < n; i++) {
-            while (j < n && temp[j] - temp[i] < nums.size()) j++;
-            result = min(result, m - (j - i));
+            while (j < n && temp[j] - temp[i] < m) j++;
+            result = min(result, m - (j - i)); // Fix: use `m - (j - i)`
         }
+        
         return result;
     }
 };
