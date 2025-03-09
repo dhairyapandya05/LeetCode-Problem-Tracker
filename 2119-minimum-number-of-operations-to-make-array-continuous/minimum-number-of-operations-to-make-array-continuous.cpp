248b9b597a4920665a02b864a0719906e66c359e
Dhairya Pandya <75428863+dhairyapandya05@users.noreply.github.com>
2025-03-10 03:17:50 +0530

    Time: 255 ms (22.64%) | Memory: 110.7 MB (42.20%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        set<int>st (nums.begin(),nums.end());
-        vector<int>temp (st.begin(),st.end());
+        set<int> st(nums.begin(), nums.end());
+        vector<int> temp(st.begin(), st.end());
 
         int n = temp.size();
+        int m = nums.size();
+
         int result = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            int L=temp[i];
-            int R=L+nums.size()-1;
-            auto itrJ=upper_bound(temp.begin(),temp.end(),R);
-            int j=itrJ-temp.begin();
-            int inrange=j-i;
-            int operation=nums.size()-inrange;
-            result = min(result, operation);
+
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j < n && temp[j] - temp[i] < nums.size()) j++;
+            result = min(result, m - (j - i));
         }
         return result;
     }
