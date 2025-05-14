fa5622d3ded51a596a906fe6fd0ddbfaf0d42c84
Dhairya Pandya <dhairyapandya05@gmail.com>
2025-05-14 18:42:23 +0530

    Time: 12 ms (97.56%) | Memory: 18.9 MB (96.59%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -5,8 +5,10 @@ public:
     int lengthAfterTransformations(string s, int t) {
         array<int, 26> counts{};
         for (char c : s) counts[c - 'a']++;
-        for (int z = 25, a = 0; t--; z = z ? z - 1 : 25, a = a ? a - 1 : 25)
+        for (int z = 25, a = 0; t--; z = z ? z - 1 : 25, a = a ? a - 1 : 25){
             counts[a] += counts[z] %= mod;
+            // cout<<"a: "<<a<<" and "<<"z: "<<z<<endl;
+        }
         return accumulate(counts.begin(), counts.end(), 0l) % mod;
     }
 };
