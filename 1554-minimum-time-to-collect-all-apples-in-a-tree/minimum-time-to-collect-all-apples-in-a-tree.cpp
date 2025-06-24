342618a950738949c46ef86a8a08bd8439f88d38
Dhairya Pandya <dhairyapandya05@gmail.com>
2025-06-24 14:50:46 +0530

    Time: 115 ms (10.07%) | Memory: 78 MB (15.16%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int dfs(int currNode,int parentNode,unordered_map<int,vector<int>>& mp,vector<bool>& hasApple){
-        int time=0;
-        
-        for(auto it:mp[currNode]){
-            if(it==parentNode) continue;
-            time+=dfs(it,currNode,mp,hasApple);
+    int dfs(int currNode, int parentNode, unordered_map<int, vector<int>>& mp,
+            vector<bool>& hasApple) {
+        int time = 0;
+
+        for (auto it : mp[currNode]) {
+            if (it == parentNode)
+                continue;
+            int timefromchild =dfs(it, currNode, mp, hasApple);
+            if ((hasApple[it] or timefromchild != 0)) {
+                time+=(timefromchild + 2);
+            }
         }
-        if((hasApple[currNode] or time!=0) and currNode!=0){
-            time+=2;
-            cout<<"CurrNode: "<<currNode<<"Time: "<<time;
-        }
+
         return time;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        unordered_map<int,vector<int>>mp;
-        for(auto it:edges){
+        unordered_map<int, vector<int>> mp;
+        for (auto it : edges) {
             mp[it[0]].push_back(it[1]);
             mp[it[1]].push_back(it[0]);
         }
-        return dfs(0,-1,mp,hasApple);
+        return dfs(0, -1, mp, hasApple);
     }
 };
