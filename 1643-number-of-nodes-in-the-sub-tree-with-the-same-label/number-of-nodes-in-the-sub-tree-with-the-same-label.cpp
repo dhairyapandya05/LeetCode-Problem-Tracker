8479c2cbc0e86d4fd28dfca77d2615bdc785f2e9
Dhairya Pandya <dhairyapandya05@gmail.com>
2025-06-24 15:34:50 +0530

    Time: 323 ms (18.87%) | Memory: 223.1 MB (13.42%) - LeetSync

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     unordered_map<int,vector<int>>mp;
-    vector<int> dfs(int node,int parent,unordered_map<int,vector<int>>&adj,string& labels){
+    vector<int> dfs(int node,int parent,unordered_map<int,vector<int>>&adj,string& labels,vector<int>& ans){
         vector<int> tempmap(26,0);
         tempmap[labels[node]-'a']++;
         for(auto it:adj[node]){
             if(it==parent) continue;
-            vector<int>temp=dfs(it,node,adj,labels);
+            vector<int>temp=dfs(it,node,adj,labels,ans);
             for(int i=0;i<26;i++){
                 tempmap[i]+=temp[i];
             }
         }
-        mp[node]=tempmap;
+        ans[node]=tempmap[labels[node]-'a'];
         return tempmap;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
@@ -20,13 +20,14 @@ public:
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        auto rootmp=dfs(0,-1,adj,labels);
-        mp[0]=rootmp;
         vector<int>ans(n,0);
-        for(int i=0;i<n;i++){
-            auto temp_mp=mp[i];
-            ans[i]=temp_mp[labels[i]-'a'];
-        }
+
+        auto rootmp=dfs(0,-1,adj,labels,ans);
+        // mp[0]=rootmp;
+        // for(int i=0;i<n;i++){
+        //     auto temp_mp=mp[i];
+        //     ans[i]=temp_mp[labels[i]-'a'];
+        // }
         return ans;
     }
 };
