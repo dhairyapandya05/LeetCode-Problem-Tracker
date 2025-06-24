41a471e82c93516578402bfb50819ed2b3a68838
Dhairya Pandya <dhairyapandya05@gmail.com>
2025-06-24 15:18:47 +0530

    Time: 898 ms (7.55%) | Memory: 373.7 MB (5.25%) - LeetSync

---
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>>mp;
+    vector<int> dfs(int node,int parent,unordered_map<int,vector<int>>&adj,string& labels){
+        vector<int> tempmap(26,0);
+        tempmap[labels[node]-'a']++;
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            vector<int>temp=dfs(it,node,adj,labels);
+            for(int i=0;i<26;i++){
+                tempmap[i]+=temp[i];
+            }
+        }
+        mp[node]=tempmap;
+        return tempmap;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        unordered_map<int,vector<int>>adj;
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        auto rootmp=dfs(0,-1,adj,labels);
+        mp[0]=rootmp;
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++){
+            auto temp_mp=mp[i];
+            ans[i]=temp_mp[labels[i]-'a'];
+        }
+        return ans;
+    }
+};
