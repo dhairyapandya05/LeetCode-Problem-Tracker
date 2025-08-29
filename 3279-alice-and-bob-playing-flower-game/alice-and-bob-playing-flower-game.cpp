25757c7f05574fa8eb123425482517f447e4bd18
Dhairya Pandya <dhairyapandya05@gmail.com>
2025-08-30 01:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (0.00%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long flowerGame(int n, int m) {
         long long ans = 0;
-        ans = ceil((double)n/2)*(m/2) + ceil( (double) m/2)*(n/2);
+        return ceil((double)n/2)*(m/2) + ceil( (double) m/2)*(n/2);
         return ans;
     }
 };
